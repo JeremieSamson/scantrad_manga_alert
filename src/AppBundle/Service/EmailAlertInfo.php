ef9b14e541ed25415a51214e966ab6afd9542dba
Jeremie Samson <jeremie.samson76@gmail.com>
2018-05-24 14:47:54 +0200

    Add nb alerts in header

---
+++ src/AppBundle/Service/EmailAlertInfo.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Service;
+
+use AppBundle\Entity\Manga;
+use Doctrine\ORM\EntityManager;
+
+class EmailAlertInfo
+{
+    /**
+     * @var string
+     */
+    private $em;
+
+    /**
+     * EmailAlertInfo constructor.
+     * @param EntityManager $em
+     */
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNbAlerts(){
+        $total = 0;
+
+        $mangas = $this->em->getRepository('AppBundle:Manga')->findAll();
+
+        /** @var Manga $manga */
+        foreach ($mangas as $manga){
+            $total += $manga->getEmailAlerts()->count();
+        }
+
+        return $total;
+    }
+}
